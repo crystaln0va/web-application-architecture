e5b92495e4c1fc722b1bdfeaef289c9b11039343
gbeatworld <ganbayar.plus@gmail.com>
2022-11-22 18:18:34 -0600

    maven config

--- src/main/java/com/example/property_management/PropertyManagementApplication.java
+++ src/main/java/com/example/property_management/PropertyManagementApplication.java
@@ -3,6 +3,7 @@ package com.example.property_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PropertyManagementApplication {
 
