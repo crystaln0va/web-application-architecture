b9f2b90849bb5183d6d053fedc9aeecfc6a049ac
gbeatworld <ganbayar.plus@gmail.com>
2022-11-24 15:05:33 -0600

    cors disbled

--- src/main/java/com/example/property_management/Security/SecurityConfiguration.java
+++ src/main/java/com/example/property_management/Security/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception{
         http
-                .cors().and()
+                .cors().disable()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/v1/authenticate/**").permitAll()
