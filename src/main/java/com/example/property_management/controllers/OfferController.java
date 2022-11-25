4abbd97ac454fa1188262ffedf1737c7c2e282d0
gbeatworld <ganbayar.plus@gmail.com>
2022-11-25 15:35:21 -0600

    offer2

--- src/main/java/com/example/property_management/controllers/OfferController.java
+++ src/main/java/com/example/property_management/controllers/OfferController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/products/offer")
+@RequestMapping("/properties/offer")
 @CrossOrigin(origins = "*",allowedHeaders = "*")
 public class OfferController {
 
