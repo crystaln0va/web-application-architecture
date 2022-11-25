f7e8c210cfacced708ce8402b18d445b5f328e57
gbeatworld <ganbayar.plus@gmail.com>
2022-11-25 15:34:17 -0600

    offer controller

--- src/main/java/com/example/property_management/controllers/OfferController.java
+++ src/main/java/com/example/property_management/controllers/OfferController.java
@@ -4,13 +4,12 @@ import com.example.property_management.Services.ReviewService;
 import com.example.property_management.entity.Review;
 import com.example.property_management.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/products/offer")
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 public class OfferController {
 
     private final ReviewService reviewService;
@@ -19,5 +18,5 @@ public class OfferController {
     public Review makeOffer(@PathVariable Long user_id, @RequestBody Review offer){
         return reviewService.addOffer(user_id,offer);
     }
-    
+
 }
